3ba3458616b26b23c00066bf21ccc1057718bd0b
nelson <chinwubanelson@gmail.com>
2025-11-30 16:33:40 +0100

    tested SSR with external endpoint

---
+++ src/app/example/page.tsx
@@ -0,0 +1,17 @@
+// export default async function ExamplePage() {
+//   const res = await fetch(
+//     "https://job-board-assessment.vercel.app/api/jobs/6865974b5d88700cfc20348c",
+//     {
+//       cache: "no-store", // optional: for always-fresh data
+//     }
+//   );
+
+//   const data = await res.json();
+//   console.log("data is", data);
+
+//   return (
+//     <div>
+//       <h1>TEST Page</h1>
+//     </div>
+//   );
+// }
